a08d55d30017d0457e29699344f8e54d9c94f002
Gabe Fernando <gef756@gmail.com>
2015-10-01 23:20:06 -0400

    DOC: add comments on sections

--- src/main/scala/AirplaneAnalysis.scala
+++ src/main/scala/AirplaneAnalysis.scala
@@ -5,21 +5,24 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object AirplaneAnalysis {
   def stdev(col: Column): Column = {
+    // Calculate standard deviation of elements in a column
     sqrt(avg(col * col) - avg(col) * avg(col))
   }
 
   def main(args: Array[String]) = {
+    // Load data sources
     val config = ConfigFactory.load()
     val creds: Config = config.getConfig("dataSources")
     val flightFile: String = creds.getString("root") + creds.getString("flightFile")
     val carrierFile: String = creds.getString("root") + creds.getString("carrierFile")
     val planeFile: String = creds.getString("root") + creds.getString("planeFile")
 
-
+    // Set up Spark Contexts
     val conf = new SparkConf().setAppName("AirplaneAnalysis")
     val sc = new SparkContext(conf)
     val sql = new SQLContext(sc)
 
+    // Read in Data
     val csvReader: DataFrameReader = sql.read
       .format("com.databricks.spark.csv")
       .options(Map("header" -> "true",
@@ -40,7 +43,7 @@ object AirplaneAnalysis {
     println("---Plane data loaded.---")
     planeData.printSchema()
 
-
+    // Slice arrival delays by month
     println("---Analysis of Delays by Month---")
     val monthlyGB: GroupedData = flightData.groupBy(flightData("Month"))
     val monthlyDelays: DataFrame = monthlyGB.agg(avg(flightData("ArrDelay")),
@@ -48,7 +51,11 @@ object AirplaneAnalysis {
 
     monthlyDelays.foreach(println)
 
-
+    // Slice arrival delays by season
+    // Season 0 (Winter): Jan, Feb, Mar
+    // Season 1 (Spring): Apr, May, Jun
+    // Season 2 (Summer): Jul, Aug, Sep
+    // Season 3 (Fall): Oct, Nov, Dec
     println("---Analysis of Delays by Season---")
     val flightDataS: DataFrame = flightData.withColumn("Season", (flightData("Month") / 4).cast(types.IntegerType))
     val seasonalDelay: DataFrame = flightDataS.groupBy(flightDataS("Season"))
